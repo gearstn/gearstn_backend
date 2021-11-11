9735830fd26cd09931f385795f248b1db7ad902b
petermalak <pmalak14@gmail.com>
2021-11-11 14:51:59 +0200

    fixing fetching data

--- app/Http/Resources/MachineResource.php
+++ app/Http/Resources/MachineResource.php
@@ -16,8 +16,22 @@ class MachineResource extends JsonResource
     {
         $data = [
             "id" => $this->id,
-            // "title_en" => $this->title_en,
-            // "title_ar" => $this->title_ar,
+            'year' => $this->year, ,
+            'sn' => $this->sn, ,
+            'condition' => $this->condition, ,
+            'hours' => $this->hours, ,
+            'description' => $this->description, ,
+            'sell_type' => $this->sell_type, ,
+            'rent_hours' => $this->rent_hours, ,
+            'country' => $this->country, ,
+            'slug' => $this->slug, ,
+            'images' => $this->images, ,
+            'approved' => $this->approved, ,
+            'seller_id' => $this->seller_id, ,
+            'category_id' => $this->category_id, ,
+            'sub_category_id' => $this->sub_category_id, ,
+            'manufacture_id' => $this->manufacture_id, ,
+            'model_id' => $this->model_id, ,
         ];
         return $data;
     }
