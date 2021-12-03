9a9ba9f4b5cf109cb8585cd9c3fcf92056705c77
petermalak <pmalak14@gmail.com>
2021-12-03 13:44:12 +0200

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,7 +20,7 @@ class Cors
                 ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', '*')
                 // ->header('Access-Control-Allow-Credentials', true)
-                ->header('Access-Control-Allow-Headers', 'Origin, Content-Type');
+                ->header('Access-Control-Allow-Headers', '*');
 
 
                 // $response = $next( $request );
