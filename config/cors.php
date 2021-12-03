ceba4c23154623f13f77b4c0d4a8cdf09023f25f
petermalak <pmalak14@gmail.com>
2021-12-03 14:45:42 +0200

    cors

--- config/cors.php
+++ config/cors.php
@@ -33,12 +33,10 @@ return [
 
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    // 'allowed_origins' => ['http://127.0.0.1:8080/', 'http://localhost:8080/'], <-- doesn't work, still gets CORS error
-    'allowed_origins' => ['*'],  // <-- it works but it should not be like that
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['content-type', 'accept', 'x-custom-header', 'Access-Control-Allow-Origin'],
-    // 'allowed_headers' => ['*'],
-    'exposed_headers' => ['x-custom-response-header'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => ['*'],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => false,
     'max_age' => 0,
     'supports_credentials' => false,
 
