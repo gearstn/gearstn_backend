1284333fd3e8d5678ecbcedf4f951bce4bad618e
petermalak <pmalak14@gmail.com>
2021-12-03 13:33:52 +0200

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,31 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
+    // 'allowed_methods' => ['*'],
+
+    // 'allowed_origins' => ['*'],
+
+    // 'allowed_origins_patterns' => ['*'],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => false,
+
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => ['*'],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
+    // 'allowed_origins' => ['http://127.0.0.1:8080/', 'http://localhost:8080/'], <-- doesn't work, still gets CORS error
+    'allowed_origins' => ['*'],  // <-- it works but it should not be like that
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['content-type', 'accept', 'x-custom-header', 'Access-Control-Allow-Origin'],
+    // 'allowed_headers' => ['*'],
+    'exposed_headers' => ['x-custom-response-header'],
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
