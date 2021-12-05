4e8615351aec84da7cbada3ef3af72e90226a910
petermalak <pmalak14@gmail.com>
2021-12-05 14:07:46 +0200

    cleaning cors comments

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,29 +16,11 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        // return $next($request)
-        //         ->header('Access-Control-Allow-Origin', '*')
-        //         ->header('Access-Control-Allow-Methods', '*')
-        //         // ->header('Access-Control-Allow-Credentials', true)
-        //         ->header('Access-Control-Allow-Headers', 'Origin, Content-Type');
-
-
-        // return $next($request)
-        // ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
-        // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        // ->header('Access-Control-Allow-Headers',' *');
-
         $response = $next($request);
         $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE');
         // $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
         $response->header('Access-Control-Allow-Headers', '*');
         $response->header('Access-Control-Allow-Origin', '*');
         return $response;
-
-                // $response = $next( $request );
-                // $response->header( 'Access-Control-Allow-Origin', '*' );
-                // $response->header( 'Access-Control-Allow-Headers', 'Origin, Content-Type' );
-
-                // return $response;
     }
 }
