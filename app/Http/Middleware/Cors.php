35dcf930ec5b0dbb355cd9f5c6d7d1059202a529
petermalak <pmalak14@gmail>
2021-12-13 12:54:25 +0200

    adding options to cors methods

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,10 +17,10 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE');
+        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE, OPTIONS');
         // $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        $response->header('Access-Control-Allow-Headers', 'https://gearstn.com, https://www.gearstn.com');
-        $response->header('Access-Control-Allow-Origin', 'https://gearstn.com , https://www.gearstn.com');
+        $response->header('Access-Control-Allow-Headers', 'https://gearstn.com');
+        $response->header('Access-Control-Allow-Origin', 'https://gearstn.com ');
         return $response;
     }
 }
