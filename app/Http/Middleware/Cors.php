cdeb74bfe33e67f075dc0892156dd153958bbeef
petermalak <pmalak14@gmail>
2021-12-13 13:00:12 +0200

    fixing cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,7 @@ class Cors
         $response = $next($request);
         $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE, OPTIONS');
         // $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        $response->header('Access-Control-Allow-Headers', 'https://gearstn.com');
+        $response->header('Access-Control-Allow-Headers', '*');
         $response->header('Access-Control-Allow-Origin', 'https://gearstn.com ');
         return $response;
     }
