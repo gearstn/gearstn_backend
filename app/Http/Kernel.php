4799aae693d154fc0d225336fcd6f1843bf4b3cc
petermalak <pmalak14@gmail>
2021-12-27 17:27:04 +0200

    fixing changing localization by headers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,12 +18,13 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
         // \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\Cors::class,
-
+        
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-
+        // \App\Http\Middleware\Localization::class,
+        \App\Http\Middleware\Localization::class,
     ];
 
     /**
