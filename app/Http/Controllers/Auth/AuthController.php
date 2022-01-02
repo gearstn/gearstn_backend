d0bf7980c3c3b6f1570dac4812311f121d4d4990
kareemmoheb <kmoheb92@gmail.com>
2022-01-02 16:25:02 +0200

    remove compan name reuire from register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         $validator = Validator::make($inputs, [
             'first_name' => 'string|max:255',
             'last_name' => 'string|max:255',
-            'company_name' => 'string|max:255',
+            // 'company_name' => 'string|max:255',
             'email' => 'required|string|email|unique:users,email',
             'password' => 'required|string|min:8|confirmed',
             'role_id' => 'required',
