e3bda710038acf0a076353638951c09ef842374a
petermalak <pmalak14@gmail>
2022-01-12 14:51:37 +0200

    return image with news

--- app/Http/Resources/NewsResource.php
+++ app/Http/Resources/NewsResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Upload;
 
 class NewsResource extends JsonResource
 {
@@ -18,7 +19,7 @@ class NewsResource extends JsonResource
             "id" => $this->id,
             "title_en" => $this->title_en,
             "title_ar" => $this->title_ar,
-            "image_id" => $this->image_id,
+            "image_id" => Upload::find($this->image_id,['id', 'url']),
             "bodytext_en" => $this->bodytext_en,
             "bodytext_ar" => $this->bodytext_ar,
             "slug" => $this->slug,
