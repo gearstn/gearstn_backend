494c45d9b9c6e0c70ffdbdfc810c985bafb9d0f4
kareemmoheb <kmoheb92@gmail.com>
2022-02-02 13:48:58 +0200

    add phone in resources

--- app/Http/Resources/NormalUserResource.php
+++ app/Http/Resources/NormalUserResource.php
@@ -19,6 +19,7 @@ class NormalUserResource extends JsonResource
             "first_name" => $this->first_name,
             "last_name" => $this->last_name,
             "company_name" => $this->company_name,
+            "phone" => $this->phone,
             "email" => $this->email,
             "country" => $this->country,
             "role" => $this->getRoleNames(),
