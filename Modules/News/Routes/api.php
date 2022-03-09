c05b99889413c43a74fcbb182f2583163d875b49
petermalak <pmalak14@gmail.com>
2022-03-09 12:46:47 +0200

    fix news api

--- Modules/News/Routes/api.php
+++ Modules/News/Routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\News\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
