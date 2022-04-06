9e91c0d9c4e1c3a9c5444daefbd6e580c02ce043
petermalak <pmalak14@gmail.com>
2022-04-06 14:32:08 +0200

    fixing transactions

--- Modules/Transaction/Http/Controllers/TransactionController.php
+++ Modules/Transaction/Http/Controllers/TransactionController.php
@@ -29,10 +29,10 @@ class TransactionController extends Controller
 
         if (isset($request->validator) && $request->validator->fails()) {
             return response([
-                'error_code'=> 'VALIDATION_ERROR',
+                'error_code' => 'VALIDATION_ERROR',
                 'message'   => 'The given data was invalid.',
                 'errors'    => $request->validator->errors()
-            ],400);
+            ], 400);
         }
 
         $inputs = $request->validated();
@@ -43,46 +43,46 @@ class TransactionController extends Controller
         $merchantCode = "siYxylRjSPx+Mv6El3ZP+Q==";
         $secrure_key = '44adc425-0a2b-4936-921a-b705102d56b8';
         $merchantRefNumber = $inputs['merchantRefNumber'];
-        $signature = hash('sha256' , $merchantCode . $merchantRefNumber . $secrure_key);
+        $signature = hash('sha256', $merchantCode . $merchantRefNumber . $secrure_key);
         $response = Http::withBasicAuth('keys', 'secret')
-        ->get('https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/status/v2?merchantCode=siYxylRjSPx+Mv6El3ZP+Q==&merchantRefNumber='. $merchantRefNumber.'&signature='. $signature);
+            ->get('https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/status/v2?merchantCode=siYxylRjSPx+Mv6El3ZP+Q==&merchantRefNumber=' . $merchantRefNumber . '&signature=' . $signature);
 
-        if( $response->successful() ){
-            $response = json_decode($response->getBody()->getContents(), true);
-            return response([
-                'message' => 'Transaction already exists in fawry',
-                'data' =>$response
-            ],420);
-        }elseif( $response->failed() ){
-
-            $inputs['fawry_order_status_id'] = OrderStatus::where('name_en',$inputs['orderStatus'])->first()->id;
+        if ($response->successful()) {
+            $inputs['fawry_order_status_id'] = OrderStatus::where('name_en', $inputs['orderStatus'])->first()->id;
             $inputs['user_id'] = User::find($inputs['customerProfileId'])->id;
-            unset($inputs['orderStatus'],$inputs['customerProfileId'],$inputs['subscription_id']);
+            unset($inputs['orderStatus'], $inputs['customerProfileId'], $inputs['subscription_id']);
 
-            if($inputs['fawry_order_status_id'] == 1){
-                $post = new POST_Caller(SubscriptionController::class,'subscribe',Request::class,$subscription_data);
+            if ($inputs['fawry_order_status_id'] == 1) {
+                $post = new POST_Caller(SubscriptionController::class, 'subscribe', Request::class, $subscription_data);
                 $response = $post->call();
-                if($response->status() != 200) { return $response; }
+                if ($response->status() != 200) {
+                    return $response;
+                }
 
                 Transaction::create($inputs);
 
                 return response()->json([
                     'message_en' => 'Transaction Created Succesfully',
                     'message_ar' => 'لقد تم تسجيل العملية بنجاح',
-                ],200);
-            }
-            else if($inputs['fawry_order_status_id'] == 2){
-                DelayedSubsctiptionJob::dispatch($inputs,$subscription_data)->delay(Carbon::now()->addDays(1));
+                ], 200);
+            } else if ($inputs['fawry_order_status_id'] == 2) {
+                DelayedSubsctiptionJob::dispatch($inputs, $subscription_data)->delay(Carbon::now()->addDays(1));
                 return response()->json([
                     'message_en' => 'Waiting to pay for your subscription',
                     'message_ar' => 'فى انتظار الدفع',
-                ],200);
+                ], 200);
             }
-        }
+        } elseif ($response->failed()) {
 
+            $response = json_decode($response->getBody()->getContents(), true);
+            return response([
+                'message' => 'Transaction already exists in fawry',
+                'data' => $response
+            ], 420);
+        }
     }
 
-        /**
+    /**
      * Store a newly created resource in storage.
      * @return Renderable
      */
@@ -90,10 +90,10 @@ class TransactionController extends Controller
     {
         if (isset($request->validator) && $request->validator->fails()) {
             return response([
-                'error_code'=> 'VALIDATION_ERROR',
+                'error_code' => 'VALIDATION_ERROR',
                 'message'   => 'The given data was invalid.',
                 'errors'    => $request->validator->errors()
-            ],400);
+            ], 400);
         }
 
 
@@ -106,51 +106,50 @@ class TransactionController extends Controller
         $merchantCode = "siYxylRjSPx+Mv6El3ZP+Q==";
         $secrure_key = '44adc425-0a2b-4936-921a-b705102d56b8';
         $merchantRefNumber = $inputs['merchantRefNumber'];
-        $signature = hash('sha256' , $merchantCode . $merchantRefNumber . $secrure_key);
+        $signature = hash('sha256', $merchantCode . $merchantRefNumber . $secrure_key);
         $response = Http::withBasicAuth('keys', 'secret')
-        ->get('https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/status/v2?merchantCode=siYxylRjSPx+Mv6El3ZP+Q==&merchantRefNumber='. $merchantRefNumber.'&signature='. $signature);
+            ->get('https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/status/v2?merchantCode=siYxylRjSPx+Mv6El3ZP+Q==&merchantRefNumber=' . $merchantRefNumber . '&signature=' . $signature);
 
-        if( $response->successful() ){
-            $response = json_decode($response->getBody()->getContents(), true);
-            return response([
-                'message' => 'Transaction already exists in fawry',
-                'data' =>$response
-            ],420);
-        }elseif( $response->failed() ){
-
-
-
-            $inputs['fawry_order_status_id'] = OrderStatus::where('name_en',$inputs['orderStatus'])->first()->id;
+        if ($response->successful()) {
+            $inputs['fawry_order_status_id'] = OrderStatus::where('name_en', $inputs['orderStatus'])->first()->id;
             $inputs['user_id'] = User::find($inputs['customerProfileId'])->id;
-            unset($inputs['orderStatus'],$inputs['customerProfileId'],$inputs['number_of_listing'],$inputs['number_of_months']);
+            unset($inputs['orderStatus'], $inputs['customerProfileId'], $inputs['number_of_listing'], $inputs['number_of_months']);
             $subscription_data['user_id'] = $inputs['user_id'];
 
-            if($inputs['fawry_order_status_id'] == 1){
-                $post = new POST_Caller(SubscriptionController::class,'extra_plan_subscribe',Request::class,$subscription_data);
+            if ($inputs['fawry_order_status_id'] == 1) {
+                $post = new POST_Caller(SubscriptionController::class, 'extra_plan_subscribe', Request::class, $subscription_data);
                 $response = $post->call();
-                if($response->status() != 200) { return $response; }
+                if ($response->status() != 200) {
+                    return $response;
+                }
 
                 Transaction::create($inputs);
 
                 return response()->json([
                     'message_en' => 'Transaction Created Succesfully',
                     'message_ar' => 'لقد تم تسجيل العملية بنجاح',
-                ],200);
-            }
-            else if($inputs['fawry_order_status_id'] == 2){
-                DelayedExtraSubsctiptionJob::dispatch($inputs,$subscription_data)->delay(Carbon::now()->addDays(1));
+                ], 200);
+            } else if ($inputs['fawry_order_status_id'] == 2) {
+                DelayedExtraSubsctiptionJob::dispatch($inputs, $subscription_data)->delay(Carbon::now()->addDays(1));
                 return response()->json([
                     'message_en' => 'Waiting to pay for your subscription',
                     'message_ar' => 'فى انتظار الدفع',
-                ],200);
+                ], 200);
             }
+        } elseif ($response->failed()) {
+
+            $response = json_decode($response->getBody()->getContents(), true);
+            return response([
+                'message' => 'Transaction already exists in fawry',
+                'data' => $response
+            ], 420);
         }
     }
 
     public function user_transactions()
     {
         $user = User::find(auth()->user()->id);
-        $transactions = Transaction::where('user_id',$user->id)->get();
+        $transactions = Transaction::where('user_id', $user->id)->get();
         return TransactionResource::collection($transactions)->additional(['status' => 200, 'message' => 'Transactions fetched successfully']);
     }
 }
