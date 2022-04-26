ff66799aa7e23d35f10fddca2b5ad05cf3fa651d
petermalak <pmalak14@gmail.com>
2022-04-26 09:22:01 +0200

    making subcategories route generic

--- Modules/SubCategory/Http/Controllers/SubCategoryController.php
+++ Modules/SubCategory/Http/Controllers/SubCategoryController.php
@@ -5,6 +5,7 @@ namespace Modules\SubCategory\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Category\Entities\Category;
 use Modules\SubCategory\Entities\SubCategory;
 use Modules\SubCategory\Http\Resources\SubCategoryResource;
 
@@ -15,9 +16,11 @@ class SubCategoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $sub_categories = SubCategory::all();
+        $category_name = $request->category;
+        $category = Category::where('title_en',$category_name)->first()->id;
+        $sub_categories = SubCategory::where('category_id',$category)->get();
         return SubCategoryResource::collection($sub_categories)->additional(['status' => 200, 'message' => 'SubCategories fetched successfully']);
     }
 
