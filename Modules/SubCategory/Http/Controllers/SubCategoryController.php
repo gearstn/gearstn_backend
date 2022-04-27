933f8889018419b3bd7246182a57ee2887521351
petermalak <pmalak14@gmail.com>
2022-04-27 11:54:25 +0200

    adding or where clause for en , ar titles

--- Modules/SubCategory/Http/Controllers/SubCategoryController.php
+++ Modules/SubCategory/Http/Controllers/SubCategoryController.php
@@ -5,6 +5,7 @@ namespace Modules\SubCategory\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Validator;
 use Modules\Category\Entities\Category;
 use Modules\SubCategory\Entities\SubCategory;
 use Modules\SubCategory\Http\Resources\SubCategoryResource;
@@ -19,7 +20,14 @@ class SubCategoryController extends Controller
     public function index(Request $request)
     {
         $category_name = $request->category;
-        $category = Category::where('title_en',$category_name)->first()->id;
+
+        $inputs = $request->all();
+        $validator = Validator::make($inputs, ['category' => 'required'] );
+        if ($validator->fails()) {
+            return response()->json($validator->messages(), 400);
+        }
+        $category_name = $inputs['category'];
+        $category = Category::where('title_en',$category_name)->orWhere('title_ar',$category_name)->first()->id;
         $sub_categories = SubCategory::where('category_id',$category)->get();
         return SubCategoryResource::collection($sub_categories)->additional(['status' => 200, 'message' => 'SubCategories fetched successfully']);
     }
