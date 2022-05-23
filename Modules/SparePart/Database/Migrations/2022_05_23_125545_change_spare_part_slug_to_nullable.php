0f91f2e3ddefcf927fb15aa8265820d7848d5cb5
petermalak <pmalak14@gmail.com>
2022-05-23 12:58:13 +0200

    adding migration for spare parts to make slug nullable

---
+++ Modules/SparePart/Database/Migrations/2022_05_23_125545_change_spare_part_slug_to_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeSparePartSlugToNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('spare_parts', function (Blueprint $table) {
+            $table->string('slug')->nullable()->change();
+        });    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('spare_parts', function (Blueprint $table) {
+            $table->string('slug')->change();
+        });    }
+}
