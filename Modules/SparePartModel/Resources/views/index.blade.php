cdde40cf437fc6d87393666293d2ea40e187d78e
petermalak <pmalak14@gmail.com>
2022-05-26 12:10:15 +0200

    removing spare parts model module

--- Modules/SparePartModel/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('sparepartmodel::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('sparepartmodel.name') !!}
-    </p>
-@endsection
