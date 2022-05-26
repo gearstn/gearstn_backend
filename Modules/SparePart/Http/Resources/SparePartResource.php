73a88980612608f8afcf7abf412524d8f170385d
petermalak <pmalak14@gmail.com>
2022-05-26 12:09:19 +0200

    modifing spare part resource on new spare part fields

--- Modules/SparePart/Http/Resources/SparePartResource.php
+++ Modules/SparePart/Http/Resources/SparePartResource.php
@@ -26,21 +26,19 @@ class SparePartResource extends JsonResource
         $selected_columns = ['id','title_en','title_ar'];
         $data = [
             "id" => $this->id,
-            'year' => $this->year,
-            'sn' => $this->sn,
             'description' => $this->description,
             'country_id' => Country::find($this->country_id,$selected_columns),
             'slug' => $this->slug,
             'images' => Upload::findMany(json_decode($this->images),['id', 'url']),
             'skq' => $this->skq,
             'price' =>  $this->price == null ? null :currency_converter('USD',$this->price),
+            'is_original' => $this->is_original,
             'approved' => $this->approved,
             'seller_id' => User::find($this->seller_id,['id','first_name', 'last_name', 'company_name', 'country', 'email' , 'phone']),
             'city_id' => City::find($this->city_id,$selected_columns),
             'category_id' => Category::find($this->category_id,$selected_columns),
             'sub_category_id' => SubCategory::find($this->sub_category_id,$selected_columns),
             'manufacture_id' => Manufacture::find($this->manufacture_id,$selected_columns),
-            'model_id' => SparePartModel::find($this->model_id,$selected_columns)->first(),
             'views' => views(SparePart::find($this->id))->count()
         ];
         return $data;
