2c708c445132389fdf0737355d85e0843e2c546c
petermalak <pmalak14@gmail.com>
2022-06-14 13:25:59 +0200

    returning images in service resource

--- Modules/Service/Http/Resources/ServiceResource.php
+++ Modules/Service/Http/Resources/ServiceResource.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use Modules\Country\Entities\Country;
 use Modules\ServiceType\Entities\ServiceType;
 use Modules\City\Entities\City;
+use Modules\Upload\Entities\Upload;
+
 class ServiceResource extends JsonResource
 {
     /**
@@ -28,6 +30,7 @@ class ServiceResource extends JsonResource
             'service_type_id' => ServiceType::find($this->service_type_id,['id','title_en', 'title_ar']),
             'city_id' => City::find($this->city_id,$selected_columns),
             'country_id' => Country::find($this->country_id,$selected_columns),
+            'images' => Upload::findMany(json_decode($this->images),['id', 'url']),
         ];
         return $data;
     }
